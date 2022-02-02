240e2328a6d99630b6eeefbb926835cf78334ccb
TrottingSpace <90799518+TrottingSpace@users.noreply.github.com>
2022-02-02 02:50:22 +0100

    2022-02-02_02-50

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,74 +1,93 @@
 //import org.jetbrains.compose.web.attributes.*
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
+//import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.attributes.max
-import org.jetbrains.compose.web.attributes.min
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import kotlin.random.Random
 
-var fieldWith = 16
-var fieldHeight = 8
-var minesCount = 37
+var fieldWidth = 32
+var fieldHeight = 16
+var minesCount = 27
 
 class Menu {
     var hidden by mutableStateOf(true)
 }
 
-class Field(private val fieldIndex: Int) {
+class Field(private val row: Int, private val col: Int) {
+    val index = (row * fieldWidth) + col
     var content = 0
         private set
+    fun placeMine() { content = 9 }
     var hidden by mutableStateOf(true)
         private set
     var flagged by mutableStateOf(false)
         private set
-    private val nameList = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!!", "?")
+    private val nameList = listOf(".", "1", "2", "3", "4", "5", "6", "7", "8", "(#)", "!!", "?")
     fun color(): CSSColorValue { return if (hidden) { Color.lightgray } else { Color.gray } }
     fun name(): String { return if (flagged) { nameList[10] } else if (hidden) { nameList[11] } else { nameList[content] } }
     fun show() { hidden = false }
     fun mineCounter() {
-        val searchMinesAt = mutableListOf((-fieldWith-1), (-fieldWith), (-fieldWith+1), (-1), (1), (fieldWith-1), (fieldWith), (fieldWith+1))
         if (content != 9) {
-            //
+            for (r in (row-1)..(row+1)) {
+                for (c in (col-1)..(col+1)) {
+                    if ((0 until fieldHeight).contains(r) && (0 until fieldWidth).contains(c)) {
+                        if (field[r][c].content == 9) { content += 1 }
+                    }
+                }
+            }
         }
     }
 }
 
 val menu = Menu()
-val field: MutableList<Field> = mutableListOf()
+val field: MutableList<MutableList<Field>> = mutableListOf()
 fun rebuildMinefield() {
     while (field.size > 0) { field.removeAt(0) }
-    for (indexF in 0 until (fieldHeight * fieldWith)) { field.add(Field(indexF)) }
+    for (i in 0 until fieldHeight) {
+        field.add(mutableListOf())
+        for (j in 0 until fieldWidth) {
+            field[i].add(Field(i, j))
+        }
+    }
 }
 
+fun minePlacerAndCounter() {
+    val fields = (0 until (fieldHeight * fieldWidth)).map { it }.toMutableList()
+    if (minesCount < fields.size) {
+        var counter = 0
+        while (counter < minesCount) {
+            val rIndex = fields[Random.nextInt(fields.size)]
+            field[rIndex/fieldWidth][rIndex%fieldWidth].placeMine()
+            fields.remove(rIndex)
+            counter += 1
+        }
+    } else {
+        console.log("Too many mines!")
+    }
+    (0 until (fieldHeight * fieldWidth)).forEach { field[it/fieldWidth][it%fieldWidth].mineCounter() }
+}
 
+fun revealAll() { (0 until (fieldHeight * fieldWidth)).forEach { field[it/fieldWidth][it%fieldWidth].show() } }
 
-
-var fieldSize = 16
 
 fun main() {
     console.log("%c Welcome in Minesweeper! ", "color: white; font-weight: bold; background-color: black;")
     rebuildMinefield()
+    minePlacerAndCounter()
 
-    console.log("Starting with:\n Field width:\t", fieldWith, "\n Field height:\t", fieldHeight, "\n Field size:\t", field.size, "\n Mines count:\t", minesCount)
+    console.log("Starting with:\n Field width:\t", fieldWidth, "\n Field height:\t", fieldHeight, "\n Field size:\t", field.size, "\n Mines count:\t", minesCount)
 
 
-
-    //calculating dimensions
-    val boxSize: Int = if (window.innerHeight < window.innerWidth){ (window.innerHeight / (fieldSize + 1)) } else { (window.innerWidth / (fieldSize + 2)) }
-    console.log(" Window height:\t", window.innerHeight, "\n Window width:\t", window.innerWidth, "\n Size factor: \t", boxSize)
-    var boxSizeMultiplier by mutableStateOf(1.0)
 
 
 
     //"Minesweeper_root" div style applying
-    //document.getElementById("Minesweeper_root")?.setAttribute("style", "padding: 0px; border: none; aspect-ratio: ${fieldWith.toDouble() / (fieldHeight + 1)}; position: relative; margin: 0px auto;")
+    document.getElementById("Minesweeper_root")?.setAttribute("style", "padding: 0px; border: none; /*aspect-ratio: ${fieldWidth.toDouble() / (fieldHeight + 1)};*/ position: relative; margin: 0px auto;")
 
     renderComposable(rootElementId = "Minesweeper_root") {
 
@@ -79,10 +98,13 @@ fun main() {
                 //
             }
             //menu bar table
-            Table({ style { width(100.percent); height(100.percent); property("aspect-ratio", "$fieldWith / 1"); property("table-layout", "fixed"); border(1.px, LineStyle.Solid, Color.white) } }) {
+            Table({ style { width(100.percent); height(100.percent); property("aspect-ratio", "$fieldWidth / 1"); property("table-layout", "fixed"); border(1.px, LineStyle.Solid, Color.white) } }) {
                 Tr {
-                    Td({ style { border(1.px, LineStyle.Solid, Color.dimgray) } }) {
-                        Text("Top bar")
+                    Td({
+                        style { border(1.px, LineStyle.Solid, Color.dimgray) }
+                        onClick { revealAll() }
+                    }) {
+                        Text("Top bar   Work In Progress")
                     }
                 }
             }
@@ -91,7 +113,7 @@ fun main() {
                 style {
                     width(100.percent)
                     height(100.percent)
-                    property("aspect-ratio", "${fieldWith.toDouble() / fieldHeight}")
+                    property("aspect-ratio", "${fieldWidth.toDouble() / fieldHeight}")
                     property("table-layout", "fixed")
                     property("border-spacing", "0px")
                     textAlign("center")
@@ -100,17 +122,20 @@ fun main() {
             }) {
                 for (i in 0 until fieldHeight) {
                     Tr {
-                        for (j in 0 until fieldWith) {
-                            val fieldNum = j + (i * fieldWith)
+                        for (j in 0 until fieldWidth) {
+                            val fieldNum = (i * fieldWidth) + j
                             Td({ style { padding(0.px); border(1.px, LineStyle.Solid, Color.black) } }) {
                                 Button({
-                                    style { width(100.percent); height(100.percent); padding(0.px); border(1.px, LineStyle.Solid, Color.white); backgroundColor(field[fieldNum].color()) }
-                                    if (field[fieldNum].hidden) {
-                                        onClick { field[fieldNum].show() }
+                                    style { width(100.percent); height(100.percent); padding(0.px); border(1.px, LineStyle.Solid, Color.white); backgroundColor(field[i][j].color()) }
+                                    if (field[i][j].hidden) {
+                                        onClick {
+                                            if (field[i][j].content == 9) { revealAll(); window.alert("BOOM!") } else { field[i][j].show() }
+                                        }
                                     }
                                 }) {
-                                    Text("#$fieldNum#")
-                                    Text(field[fieldNum].name())
+                                    //Text("#$fieldNum#")
+                                    Text(field[i][j].name())
+                                    //Text("${field[i][j].index}")
                                 }
                             }
                         }
