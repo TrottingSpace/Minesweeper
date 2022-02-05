4982296443ffd319e1998f1b14145a863b0b022e
TrottingSpace <90799518+TrottingSpace@users.noreply.github.com>
2022-02-05 22:05:40 +0100

    2022-02-05_22-05

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -4,14 +4,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.jetbrains.compose.web.attributes.colspan
+import org.jetbrains.compose.web.attributes.*
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 import kotlin.random.Random
 
-var fieldWidth = 8
-var fieldHeight = 8
+var fieldWidth by mutableStateOf(8)
+var fieldHeight by mutableStateOf(8)
 var minesCount = 10
 
 var inGame by mutableStateOf(true)
@@ -24,6 +24,28 @@ var colorShown by mutableStateOf(Color.gray)
 
 class Menu {
     var hidden by mutableStateOf(true)
+    var newWidth = 8
+    var newHeight = 8
+    var newMines = 10
+    fun rebuild() {
+        if ((newWidth * newHeight) > 1024) { window.alert(" ${newWidth * newHeight} fields will be generated ") }
+        if (newMines >= (newWidth * newHeight)) {
+            newMines = (fieldWidth * fieldHeight) - 1
+            window.alert(" There's too many mines, reducing to $newMines ")
+        } else {
+            minesCount = newMines
+        }
+        fieldWidth = newWidth
+        fieldHeight = newHeight
+        minesCount = newMines
+        hidden = true
+        rebuildMinefield()
+        minePlacerAndCounter()
+        colorHidden = Color.lightgray
+        colorShown = Color.gray
+        consoleCurrentValues()
+        inGame = true
+    }
 }
 
 class Field(private val indexF: Int) {
@@ -115,6 +137,7 @@ fun minePlacerAndCounter() {
 
 fun revealAll() { (0 until field.size).forEach { field[it].show() } }
 
+fun consoleCurrentValues() { console.log("Playing with:\n Field width:\t", fieldWidth, "\n Field height:\t", fieldHeight, "\n Field size:\t", field.size, "\n Mines count:\t", minesCount) }
 
 fun main() {
     console.log("%c Welcome in Minesweeper! ", "color: white; font-weight: bold; background-color: black;")
@@ -125,7 +148,9 @@ fun main() {
 
 
 
-    val myFontSize = "xxx-large"
+    //document.body?.style?.backgroundColor = "green"
+
+    val myFontSize = "xx-large"
 
     //"Minesweeper_root" div style applying
     document.getElementById("Minesweeper_root")?.setAttribute("style", "padding: 0px; border: none; /*aspect-ratio: ${fieldWidth.toDouble() / (fieldHeight + 1)};*/ position: relative; margin: 0px auto;")
@@ -144,18 +169,68 @@ fun main() {
 
         Div({ style { padding(5.px); position(Position.Relative) } }) {
             //menu div
-            Div({ style { if (menu.hidden) { display(DisplayStyle.None) }; width(20.percent); height(20.percent); position(Position.Absolute); top(50.percent); left(50.percent); property("transform", "translate(-50%, -50%)") } }) {
+            Div({
+                style {
+                    if (menu.hidden) { display(DisplayStyle.None) }
+                    width(50.percent)
+                    /*height(50.percent)*/
+                    position(Position.Absolute)
+                    top(25.percent)
+                    left(50.percent)
+                    property("transform", "translate(-50%, -50%)")
+                    border(2.px, LineStyle.Solid, Color.black)
+                    backgroundColor(Color.gray)
+                    textAlign("center")
+                }
+            }) {
+                Button({ style { width(70.percent); border(2.px, LineStyle.Solid, Color.black); property("aspect-ratio", "10"); backgroundColor(Color.lightgray); property("font-size", myFontSize); margin(1.percent) } }) { Text("⚙ Menu ⚙") }
+                Input(InputType.Number) {
+                    style { border(2.px, LineStyle.Solid, Color.black); width(30.percent); property("aspect-ratio", "2"); backgroundColor(Color.lightgray); margin(1.percent); property("font-size", myFontSize) }
+                    min("2")
+                    max("32")
+                    defaultValue(fieldWidth)
+                    onChange {
+                        menu.newWidth = it.value as Int
+                        if ((menu.newWidth * menu.newHeight) > 1024) { window.alert(" There's over 1024 fields \n It may take long time to load ") }
+                        //console.log(menu.newWidth)
+                    }
+                }
+                Input(InputType.Number) {
+                    style { border(2.px, LineStyle.Solid, Color.black); width(30.percent); property("aspect-ratio", "2"); backgroundColor(Color.lightgray); margin(1.percent); property("font-size", myFontSize) }
+                    min("2")
+                    max("32")
+                    defaultValue(fieldHeight)
+                    onChange {
+                        menu.newHeight = it.value as Int
+                        if ((menu.newWidth * menu.newHeight) > 1024) { window.alert(" There's over 1024 fields \n It may take long time to load ") }
+                        //console.log(menu.newHeight)
+                    }
+                }
+                Input(InputType.Number) {
+                    style { border(2.px, LineStyle.Solid, Color.black); width(30.percent); property("aspect-ratio", "2"); backgroundColor(Color.lightgray); margin(1.percent); property("font-size", myFontSize) }
+                    min("2")
+                    defaultValue(minesCount)
+                    onChange {
+                        menu.newMines = it.value as Int
+                        if (menu.newMines > (menu.newWidth * menu.newHeight)) { window.alert(" ${menu.newMines} is too many mines for ${menu.newWidth * menu.newHeight} fields ") }
+                        //console.log(menu.newMines)
+                    }
+                }
                 Button({
-                    style { border(2.px, LineStyle.Solid, Color.indigo); width(100.percent); height(100.percent) }
+                    style { border(2.px, LineStyle.Solid, Color.black); width(32.percent); property("aspect-ratio", "3"); backgroundColor(Color.lightgray); margin(1.percent); property("font-size", myFontSize) }
+                    onClick { menu.rebuild() }
+                }) { Text("Rebuild") }
+                Button({
+                    style { border(2.px, LineStyle.Solid, Color.black); width(32.percent); property("aspect-ratio", "3"); backgroundColor(Color.lightgray); margin(1.percent); property("font-size", myFontSize) }
                     onClick { menu.hidden = true }
-                }) { Text("✅") }
+                }) { Text("Cancel") }
             }
             //menu bar table
             Table({
                 style {
                     width(100.percent)
                     height(100.percent)
-                    property("aspect-ratio", "max(6, $fieldWidth)")
+                    property("aspect-ratio", "max(7, $fieldWidth)")
                     property("table-layout", "fixed")
                     property("border-spacing", "0px")
                     border(1.px, LineStyle.Solid, Color.white)
@@ -205,20 +280,28 @@ fun main() {
                             Text("⚙")
                         }
                     }
-                    if (fieldWidth > 6) {
+                    if (fieldWidth > 7) {
                         Td({
                             style { padding(0.px); border(1.px, LineStyle.Solid, Color.black) }
-                            colspan(fieldWidth - 6)
+                            colspan(fieldWidth - 7)
                         }) {
                             Button({
                                 style { width(100.percent); height(100.percent); padding(0.px); border(1.px, LineStyle.Solid, Color.white); backgroundColor(Color.lightgray); property("font-size", myFontSize) }
-                                onClick {
+                                /*onClick {
                                     //console.log(winCounter)
                                     window.alert("This \"button\" shows You this message.\n Nothing else :)")
-                                }
+                                }*/
                             }) {
                                 Text("*")
                             }
+                        }
+                    }
+                    Td({
+                        style { padding(0.px); border(1.px, LineStyle.Solid, Color.black) }
+                        onClick { menu.rebuild() }
+                    }) {
+                        Button({ style { width(100.percent); height(100.percent); padding(0.px); border(1.px, LineStyle.Solid, Color.white); backgroundColor(Color.lightgray); property("font-size", myFontSize) } }) {
+                            Text("🔁")
                         }
                     }
                 }
@@ -228,7 +311,7 @@ fun main() {
                 style {
                     width(100.percent)
                     height(100.percent)
-                    property("aspect-ratio", "${fieldWidth.toDouble() / fieldHeight}")
+                    property("aspect-ratio", "${fieldWidth.toFloat() / fieldHeight}")
                     property("table-layout", "fixed")
                     property("border-spacing", "0px")
                     textAlign("center")
@@ -239,7 +322,7 @@ fun main() {
                     Tr {
                         for (j in 0 until fieldWidth) {
                             val fNum = (i * fieldWidth) + j
-                            Td({ style { padding(0.px); border(1.px, LineStyle.Solid, Color.black) } }) {
+                            Td({ style { height((100f/fieldHeight).percent); width((100f/fieldWidth).percent); padding(0.px); border(1.px, LineStyle.Solid, Color.black) } }) {
                                 Button({
                                     style { width(100.percent); height(100.percent); padding(0.px); border(1.px, LineStyle.Solid, Color.white); backgroundColor(field[fNum].color()); property("font-size", myFontSize) }
                                     if (field[fNum].hidden && inGame) {
